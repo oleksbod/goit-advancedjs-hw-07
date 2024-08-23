473d96695926aeb8cc0c2618774fb9a916a1e5c9
Oleksandr <oleks.bodnar@gmail.com>
2024-08-23 14:38:27 +0300

    updates

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+// Клас Key
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    private tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log(`${person.getKey().getSignature()} has entered the house.`);
+      } else {
+        console.log("The door is closed. Cannot enter.");
+      }
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log("The door is now open.");
+      } else {
+        console.log("Wrong key. The door remains closed.");
+      }
+    }
+  }
+
 const key = new Key();
 
 const house = new MyHouse(key);
